be320b7f5e95f2f3a999a177841b8cf07cd69d5e
Andrey <159928617+dissimilated2003@users.noreply.github.com>
2025-10-27 10:19:27 +0300

    габдр тчт щущу

---
+++ Trial 1 - Horse In Coat/Solution/HorseInCoat.pas
@@ -0,0 +1,104 @@
+﻿PROGRAM HorseInCoat;
+CONST
+  KeyWord = 'Кто';
+VAR
+  Input, Output: TEXT;
+  InputFile, OutputFile: STRING;
+  Sentence, LetBegin: STRING;
+  Flag, FlagOne, I, J: INTEGER;
+  Ch, ChEnd: CHAR;
+ //Основная программа
+BEGIN
+  WRITE('Введите входной файл: ');
+  READLN(InputFile);
+  ASSIGN(Input, InputFile + '.txt');
+  RESET(Input);
+  Flag:= 0;
+  WHILE NOT EOF(Input) //сканируем на предложения
+  DO
+    BEGIN
+      READ(Input, Ch);
+      IF (Ch = '.') OR (Ch = '?') OR (Ch = '!') 
+      THEN 
+        BEGIN
+          Flag := Flag + 1;
+          ChEnd := Ch;
+        END;
+    END;
+  RESET(Input); 
+  WHILE NOT EOF(Input) // рассмотрение последнего
+  DO
+    BEGIN
+      READ(Input, Ch);
+      IF (Ch = '.') OR (Ch = '?') OR (Ch = '!') 
+      THEN 
+        FlagOne := FlagOne + 1;
+      IF FlagOne = Flag - 1
+      THEN
+        BEGIN
+          WHILE FlagOne <> Flag
+          DO
+            BEGIN
+              IF EOLN(Input)
+              THEN
+                READLN(Input, Ch)
+              ELSE
+                BEGIN
+                  READ(Input, Ch);
+                  Sentence := Sentence + Ch;
+                  IF (Ch = '.') OR (Ch = '?') OR (Ch = '!') 
+                  THEN 
+                    BEGIN
+                      FlagOne := FlagOne + 1;
+                      BREAK;
+                    END
+                END          
+            END
+        END
+    END;
+  CLOSE(Input);  
+  WRITE('Введите выходной файл: ');
+  READ(OutputFile);
+  ASSIGN(Output, OutputFile + '.txt');
+  REWRITE(Output);
+  // WRITELN(ChEnd);
+  // WRITELN(Sentence);
+  IF ChEnd = '?'
+  THEN
+    BEGIN
+      FOR I := 1 TO LENGTH(Sentence) // этот цикл выносит первое слово в переменную вдруг оно будет не Кто а какое либо другое 
+      DO
+        BEGIN
+          IF Sentence[I] = ' '
+          THEN
+          ELSE
+            IF Sentence[I] <> ' '
+            THEN
+              BEGIN
+                J := I;
+                LetBegin := LetBegin + Sentence[I];
+                IF LetBegin = KeyWord
+                THEN
+                  BREAK
+              END 
+        END;
+          IF LetBegin = KeyWord
+          THEN
+            BEGIN
+              WRITE(Output, 'Конь в пальто');
+              FOR I := J + 1 TO LENGTH(Sentence) - 1 
+              DO
+                BEGIN
+                  WRITE(Output, Sentence[I])
+                END;
+              WRITE(Output, '!')
+            END
+          ELSE
+            WRITE(Output, 'Спасибо за информацию');
+    END
+  ELSE
+    WRITE(Output, 'Спасибо за информацию');
+  CLOSE(Output);
+  WRITELN;
+  WRITELN('Ответ записан в ', OutputFile, '.txt')
+END.
---
+++ Trial 1 - Horse In Coat/Solution/HorseMajor.pas
@@ -0,0 +1,55 @@
+﻿// ВОЛИК АНДРЕЙ ОЛЕГОВИЧ ПС-23
+
+//    23. В текстовом  файле  имеется  некоторое   информационнное
+// письмо. Требуется сформировать ответ на письмо. Если  последнее
+// предложение письма является  вопросом,  начинающимся  со  слова 
+// "кто", то слово "кто"  заменяется  словами  "конь в пальто",  а 
+// знак вопроса заменяется на знак восклицания. В противном случае
+// нужно дать ответ: "Спасибо за информацию" (8).
+
+PROGRAM HorseMajor;
+VAR
+  FIn, FOut: TEXT;
+  S, T, Last: STRING;
+  FInName, FOutName: STRING
+  DelimSents, DelimWords: SET OF CHAR;
+  I, Len: INTEGER;
+FUNCTION CopyStr(const Source: string; StartPos, Length: Integer): string;
+BEGIN
+  // Проверяем, что начальная позиция не меньше 1 и не больше длины строки
+  IF (StartPos < 1) OR (StartPos > Length(Source)) 
+  THEN
+    CopyStr := ''  // Если начальная позиция неверная, возвращаем пустую строку
+  ELSE 
+    IF Length < 0 
+    THEN
+      CopyStr := ''  // Если длина отрицательная, возвращаем пустую строку
+  ELSE IF StartPos + Length - 1 > Length(Source) THEN
+    CopyStr := Copy(Source, StartPos, Length(Source) - StartPos + 1)  // Корректируем длину
+  ELSE
+    CopyStr := Copy(Source, StartPos, Length);  // Возвращаем подстроку
+END;
+
+
+BEGIN
+  WRITE('Имя входного файла: ');
+  READLN(FInName);
+  IF FInName = ''
+  THEN
+    BEGIN
+      WRITELN('Ошибка: пустой ввод');
+      EXIT
+    END;
+  WRITE('Имя выходного файла: ');
+  READLN(FOutName);
+  IF FOutName = ''
+  THEN
+    BEGIN
+      WRITELN('Ошибка: пустой ввод');
+      EXIT
+    END;
+  ASSIGN(FIn, FInName + '.txt');
+  RESET(FIn);
+  ASSIGN(FOut, FOutName + '.txt');
+    
+END.
