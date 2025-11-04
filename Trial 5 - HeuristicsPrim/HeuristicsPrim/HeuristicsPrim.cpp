6726203c5eb7b8636a16258647d8051cc9b0b351
Andrey <159928617+dissimilated2003@users.noreply.github.com>
2025-11-04 16:21:00 +0300

    в волшебной стране говнокода

--- Trial 5 - HeuristicsPrim/HeuristicsPrim/HeuristicsPrim.cpp
+++ Trial 5 - HeuristicsPrim/HeuristicsPrim/HeuristicsPrim.cpp
@@ -11,19 +11,22 @@
 #include <limits>
 #include <string>
 
-using namespace std;
+using namespace std;    // вот зачем эта параша тут??
 
-int computeLoopCost(const vector<int>& tour, const vector<vector<int>>& adj) {
+int computeLoopCost(const vector<int>& tour, const vector<vector<int>>& adj) 
+{
     if (tour.size() < 2) return 0;
     int cost = 0;
-    for (int i = 0; i < (int)tour.size() - 1; i++) {
+    for (int i = 0; i < (int)tour.size() - 1; i++) 
+    {
         cost += adj[tour[i]][tour[i + 1]];
     }
     cost += adj[tour.back()][tour.front()];
     return cost;
 }
 
-int main() {
+int main()     // int ужос
+{
     setlocale(LC_ALL, "Rus");
     cout << "Имя входного файла: ";
     string filename{};
@@ -32,23 +35,29 @@ int main() {
 
     int n{};
     infile >> n;
-    if (n < 2) {
+    if (n < 2) 
+    {
         cerr << "Вершин меньше чем 2\n";
         return 1;
     }
 
     vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
-    for (int i = 0; i < n * (n - 1) / 2; i++) {
+    for (int i = 0; i < n * (n - 1) / 2; i++) 
+    {
         int u{}, v{}, w{};
         infile >> u >> v >> w;
         adj[u][v] = w;
         adj[v][u] = w;
     }
+    
     int minU = 1, minV = 2;
     int minW = adj[1][2];
-    for (int i = 1; i <= n; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            if (adj[i][j] < minW) {
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = i + 1; j <= n; j++) 
+        {
+            if (adj[i][j] < minW) 
+            {
                 minW = adj[i][j];
                 minU = i;
                 minV = j;
@@ -67,14 +76,19 @@ int main() {
     visited[minU] = true;
     visited[minV] = true;
     int visitedCount = 2;
-    while (visitedCount < n) {
+    while (visitedCount < n) 
+    {
         int bestVertex = -1;
         int bestEdgeWeight = numeric_limits<int>::max();
 
-        for (int v = 1; v <= n; v++) {
-            if (!visited[v]) {
-                for (int loopVert : tour) {
-                    if (adj[v][loopVert] < bestEdgeWeight) {
+        for (int v = 1; v <= n; v++) 
+        {
+            if (!visited[v]) 
+            {
+                for (int loopVert : tour) 
+                {
+                    if (adj[v][loopVert] < bestEdgeWeight) 
+                    {
                         bestEdgeWeight = adj[v][loopVert];
                         bestVertex = v;
                     }
@@ -84,13 +98,15 @@ int main() {
 
         int insertPos = 0;
         int bestIncrease = numeric_limits<int>::max();
-        for (int i = 0; i < (int)tour.size(); i++) {
+        for (int i = 0; i < (int)tour.size(); i++) 
+        {
             int j = (i + 1) % tour.size(); 
             int v_i = tour[i];
             int v_j = tour[j];
 
             int increase = adj[v_i][bestVertex] + adj[bestVertex][v_j] - adj[v_i][v_j];
-            if (increase < bestIncrease) {
+            if (increase < bestIncrease) 
+            {
                 bestIncrease = increase;
                 insertPos = j;
             }
@@ -102,7 +118,8 @@ int main() {
 
         int currentCost = computeLoopCost(tour, adj);
         cout << "Вставленная вершина: " << bestVertex << ". Текущий цикл: ";
-        for (int v : tour) {
+        for (int v : tour) 
+        {
             cout << v << "-";
         }
         cout << tour[0] << ". Стоимость цикла: " << currentCost << "\n";
@@ -110,10 +127,12 @@ int main() {
 
     int finalCost = computeLoopCost(tour, adj);
     cout << "Путь коммивояжера: ";
-    for (int v : tour) {
+    for (int v : tour) 
+    {
         cout << v << "-";
     }
     cout << tour[0] << ". Стоимость цикла: " << finalCost << "\n";
 
     return 0;
 }
+
