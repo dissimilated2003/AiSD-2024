db7b3dff9598cd60b6c8422b07a50f2530076084
Andrey <159928617+dissimilated2003@users.noreply.github.com>
2025-11-04 16:12:21 +0300

    ретурн 0

--- Trial 4 - MaxGruzGraph/MaxGruzGraph/MaxGruzGraph.cpp
+++ Trial 4 - MaxGruzGraph/MaxGruzGraph/MaxGruzGraph.cpp
@@ -12,27 +12,33 @@
 #include <queue>
 #include <sstream>
 
-using std::string;
+using std::string; // уаааа что за говно
 
 const int PLUS_INF = 100000000;
 const int MINUS_INF = -100000000;
 
-struct City {
+struct City 
+{
 	int CityNum;
 	string CityName;
 	std::vector<int> CityPathWeight;
 };
 
-void initPaths(std::vector<City>& cityMatrix) {
-	for (int i = 0; i < cityMatrix.size(); i++) {
-		for (int j = 0; j < cityMatrix.size(); j++) {
+void initPaths(std::vector<City>& cityMatrix) 
+{
+	for (int i = 0; i < cityMatrix.size(); i++) 
+	{
+		for (int j = 0; j < cityMatrix.size(); j++) 
+		{
 			cityMatrix[i].CityPathWeight.push_back(0);
 		}
 	}
 }
 
-void readPathWeight(std::ifstream& pathWeights, std::vector<City>& cityMatrix) {
-	while (!pathWeights.eof()) {
+void readPathWeight(std::ifstream& pathWeights, std::vector<City>& cityMatrix) 
+{
+	while (!pathWeights.eof()) 
+	{
 		char temp;
 		int baseCity;
 		int destinationCity;
@@ -47,9 +53,11 @@ void readPathWeight(std::ifstream& pathWeights, std::vector<City>& cityMatrix) {
 	}
 }
 
-std::vector<City> readCityList(std::ifstream& citylist) {
+std::vector<City> readCityList(std::ifstream& citylist) 
+{
 	std::vector<City>  cityMatrix;
-	while (!citylist.eof()) {
+	while (!citylist.eof()) 
+	{
 		City temp;
 		citylist >> temp.CityNum;
 		citylist >> temp.CityName;
@@ -58,9 +66,8 @@ std::vector<City> readCityList(std::ifstream& citylist) {
 	return cityMatrix;
 }
 
-void navigate(const std::vector<City>& cityMatrix, 
-			   int baseCity,
-			   int destinationCity) {
+void navigate(const std::vector<City>& cityMatrix, int baseCity, int destinationCity) 
+{
 	std::vector<int> path(cityMatrix.size(), 0);
 	std::vector<int> distance(cityMatrix.size(), 0);
 	std::vector<char> visited(cityMatrix.size(), 0);
@@ -68,32 +75,40 @@ void navigate(const std::vector<City>& cityMatrix,
 	minPathWeight = 0;
 	string buff, finalOutStr, midStr;
 
-	for (int i = 0; i < cityMatrix.size(); i++) {
+	for (int i = 0; i < cityMatrix.size(); i++) 
+	{
 		distance[i] = MINUS_INF;
 		path[i] = 0;
 		visited[i] = false;
 	}
 
 	distance[baseCity] = PLUS_INF;
-	for (int i = 0; i < cityMatrix.size(); i++) {
+	for (int i = 0; i < cityMatrix.size(); i++) 
+	{
 		maxWeight = MINUS_INF;
-		for (int j = 0; j < cityMatrix.size(); j++) {
-			if (!visited[j] && distance[j] >= maxWeight) {
+		for (int j = 0; j < cityMatrix.size(); j++) 
+		{
+			if (!visited[j] && distance[j] >= maxWeight) 
+			{
 				maxWeight = distance[j];
 				currVertex = j;
 			}
 		}
 		visited[currVertex] = true;
-		if (distance[currVertex] == MINUS_INF) {
+		if (distance[currVertex] == MINUS_INF) 
+		{
 			continue;
 		}
-		for (int j = 0; j < cityMatrix.size(); j++) {
-			if (visited[j]) {
+		for (int j = 0; j < cityMatrix.size(); j++) 
+		{
+			if (visited[j]) 
+			{
 				continue;
 			}
 			int weight = cityMatrix[currVertex].CityPathWeight[j];
 			int maximum = std::min(distance[currVertex], weight);
-			if (weight != 0 && maximum > distance[j]) {
+			if (weight != 0 && maximum > distance[j]) 
+			{
 				distance[j] = maximum;
 				path[j] = currVertex;
 			}
@@ -102,10 +117,12 @@ void navigate(const std::vector<City>& cityMatrix,
 
 	minPathWeight = distance[destinationCity];
 
-	if (minPathWeight != MINUS_INF) {
+	if (minPathWeight != MINUS_INF) 
+	{
 		foundVertex = path[destinationCity];
 		finalOutStr = "Путь до города назначения: ";
-		while (foundVertex != 0) {
+		while (foundVertex != 0) 
+		{
 			buff = std::to_string(foundVertex + 1);
 			midStr = cityMatrix[foundVertex].CityName + " (" + buff + ") " + "-> " + midStr;
 			foundVertex = path[foundVertex];
@@ -118,16 +135,20 @@ void navigate(const std::vector<City>& cityMatrix,
 		std::cout << finalOutStr << "\n";
 	}
 	
-	if (minPathWeight == MINUS_INF) {
+	if (minPathWeight == MINUS_INF) 
+	{
 		std::cout << "Максимальный груз из " << cityMatrix[baseCity].CityName;
 		std::cout << " в " << cityMatrix[destinationCity].CityName << " невычислим" << "\n";
-	} else {
+	} 
+	else 
+	{
 		std::cout << "Максимальный груз из " << cityMatrix[baseCity].CityName;
 		std::cout << " в " << cityMatrix[destinationCity].CityName << ": " << minPathWeight << "\n";
 	}
 }
 
-int main() {
+int main() 
+{
 	setlocale(LC_ALL, "Rus");
 	string cities{};
 	string cityRels{};
@@ -149,4 +170,5 @@ int main() {
 	std::cin >> destinationCity;
 	
 	navigate(CityMatrix, baseCity - 1, destinationCity - 1);
+	return 0;
 }
